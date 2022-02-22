c5ac35c501b59e6d7801bb733bcaa3f2864e42f7
Olumuyiwa Osiname <oluosiname@gmail.com>
2022-02-22 22:30:43 +0100

    design login and sign up page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+
 module.exports = {
   content: [
     './app/helpers/**/*.rb',
@@ -7,9 +8,19 @@ module.exports = {
     './app/views/**/*'
   ],
   theme: {
+    colors: {
+      'blue': '#FF0000',
+      'black-100': '#313131',
+      'gray-100': '#dedede',
+      'gray-400': '#616161',
+      'green-200': '#6fca3d',
+      'white': '#FFFFFF',
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        jost: ['Jost', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif']
       },
     },
   },
